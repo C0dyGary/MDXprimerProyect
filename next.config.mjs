37cbc4081abf3c9e2473def402eb3e17ba718d9e
Gary Rosa Valencia <asenvhale@gmail.com>
2022-12-16 16:50:58 -0400

    primier commit

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+import nextMDX from "@next/mdx";
+
+import remarkFrontmatter from "remark-frontmatter";
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+
+  options: {
+    remarkPlugins: [remarkFrontmatter],
+
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX({
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
